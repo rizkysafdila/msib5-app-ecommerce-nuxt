597e7930f1b885e00f04f3585d55e032d442c107
rizkysafdila <rizkysafdila@gmail.com>
2023-09-18 09:25:46 +0700

    chore(checkout.index): update some code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,8 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const cartStore = useCartStore()
+
+if (cartStore.cartProducts.length == 0) await cartStore.getAllCarts()
+</script>
 
 <template>
   <div class="bg-gradient-to-b from-[#D8EEFD] to-white">
